ca7c67f6488dd2eadbeb84f99f7694b0c8745439
Erik Lundin <im.erik.lundin@gmail.com>
2016-10-31 00:49:43 +0100

    testing simple REST API. Created a Slack slash-command resource that scrapes a dynamic content website, using the Nightmare module. 404 page for unhandled routes.

---
+++ code/slack.js
@@ -0,0 +1,73 @@
+var request = require("request");
+var cheerio = require("cheerio");
+var Promise = require('q').Promise;
+var Nightmare = require('nightmare');
+var nightmare = Nightmare();
+
+module.exports = {
+    commands: {
+        slumpvard: function () {
+            console.log("Slumpvard command triggered");
+            return Promise.resolve(
+                nightmare
+                    .goto('http://slumpvard.se')
+                    .evaluate(function () {
+                        var obj = {};
+                        obj.imageName = $('.js-insultVard').text();
+                        obj.imageUrl = $('.js-imageVard')
+                            .css("background-image");
+                        return obj;
+                    }))
+                .then(function (result) {
+
+                    var myString = result.imageUrl;
+                    var myRegexp = /url\("(http.*\.\w+)"\)/g;
+                    var match = myRegexp.exec(myString);
+                    var url = (match[1]);
+
+                    return {
+                        'attachments': [{
+                            title: "slumpvard.se",
+                            title_link: "http://slumpvard.se",
+                            text: result.imageName + "vard",
+                            image_url: url
+                                .replace('url("', '')
+                                .replace('")', ''),
+                            color: "#000",
+                            fallback: result.imageName + "vard image"
+                        }]
+                    };
+                }, function (err) {
+                    console.error(err); // notice that `throw`ing in here doesn't work
+                });
+        }
+    }
+};
+
+// regular scraping
+// var imageName = "noname";
+// var imageUrl = "https://placeholdit.imgix.net/~text?txtsize=33&txt=SLUMP&w=200&h=100";
+//
+// request("http://slumpvard.se", function(error, response, body) {
+//     var $ = cheerio.load(body);
+//
+//     var vard;
+//     $('.js-insultVard').filter(function() {
+//         vard = $(this).text().trim();
+//     });
+//     console.log(vard);
+//
+//     // imageName = $('.js-insultVard').text() + "vard";
+//     // console("name: " + imageName);
+//     //
+//     // $('.js-imageVard').each(function(i, element) {
+//     // console.log($(this).prev());
+//     // console.log($(this).attr());
+//     // console.log($(this).css("background-image"));
+//     //     imageUrl = $(this).css("background-image");
+//     //     console.log(imageUrl);
+//     // });
+//     //
+//     // //var bgImg = $('.js-imageVard').css("background-image");
+//     // var imageUrl = bgImg.replace("url(", "").replace(")", "").replace("\'", "").replace("\"", "");
+// });
--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 var express = require('express');
 var app = express();
+var slack = require('./code/slack');
+var Promise = require('q').Promise;
+var Nightmare = require('nightmare');
+var nightmare = Nightmare();
 
+// default Heroku setup
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// routes
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
+});
+
+app.get('/slack_commands/slumpvard', function(request, response) {
+    // TODO: check if there's a way to dynamically route the last part of the url to a slack command, like slack_commands/<variable> and match that to slack.<variable>()
+    var promise = slack.commands.slumpvard();
+    promise.then(function(result){
+        debugger;
+        response.json(result);
+    });
+});
+
+// 404 handling
+app.use(function(req, res, next) {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.render('pages/404', {
+            url: req.url
+        });
+        return;
+    }
+    if (req.accepts('json')) {
+        res.json({
+            error: '404 Not found'
+        });
+        return;
+    }
+    // default to plain-text. send()
+    res.type('txt').send('404 Not found');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
