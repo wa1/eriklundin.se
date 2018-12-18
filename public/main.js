37548aee03c2430656f7e8534a5637e18384517a
Erik Lundin <im.erik.lundin@gmail.com>
2018-12-18 22:59:25 +0100

    ticks info popover

--- public/main.js
+++ public/main.js
@@ -1,3 +1,7 @@
-$(function() {
+$(function () {
     new Clipboard('.clipboard-btn');
+
+    $('[data-toggle="popover"]').popover();
+
+    console.log('%cContact me through ' + '%cim.erik.lundin' + '%c at Google\'s email service', '','color:red; background-color:white; font-size:x-large','');
 });
